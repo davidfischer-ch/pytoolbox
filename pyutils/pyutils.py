66c827789b4f6ba438809f787f734e9fe52cd7e4
David Fischer <david.fischer.ch@gmail.com>
2013-05-24 17:02:30 +0200

    Add some mock utils

--- pyutils/pyutils.py
+++ pyutils/pyutils.py
@@ -28,6 +28,7 @@
 import errno, inspect, json, logging, logging.handlers, pickle, os, re, shlex, subprocess, sys, uuid
 from datetime import datetime
 from ipaddr import IPAddress
+from mock import Mock
 
 # FIXME bson.json_util is required to import dumps, loads for applications using MongoDB !
 
@@ -441,6 +442,48 @@ def cmd(command, input=None, cli_input=None, fail=True, log=None):
         if log is not None:
             log(result)
         raise subprocess.CalledProcessError(process.returncode, command, stderr)
+    return result
+
+
+# TEST / MOCK --------------------------------------------------------------------------------------
+
+MOCK_SIDE_EFFECT_RETURNS = [Exception('you must set MOCK_SIDE_EFFECT_RETURNS'), {'title': 'second'}]
+
+
+def mock_cmd(stdout='', stderr='', returncode=0):
+    return Mock(return_value={'stdout': stdout, 'stderr': stderr, 'returncode': returncode})
+
+
+def mock_side_effect(*args, **kwargs):
+    u"""
+    Pop and return values from MOCK_SIDE_EFFECT_RETURNS.
+
+    from your own module, you need to set MOCK_SIDE_EFFECT_RETURNS before using this method::
+
+        import pyutils.pyutils
+        pyutils.pyutils.MOCK_SIDE_EFFECT_RETURNS = ['first', {'title': 'second'}, EOFError('last')]
+
+    **example usage**:
+
+    Set content (only required for this doctest, see previous remark):
+
+    Pops return values with ``mock_side_effect``:
+
+    >>> print(mock_side_effect())
+    Traceback (most recent call last):
+    ...
+    Exception: you must set MOCK_SIDE_EFFECT_RETURNS
+    >>> print(mock_side_effect())
+    {'title': 'second'}
+    >>> print(mock_side_effect())
+    Traceback (most recent call last):
+    ...
+    IndexError: pop from empty list
+    """
+    global MOCK_SIDE_EFFECT_RETURNS
+    result = MOCK_SIDE_EFFECT_RETURNS.pop(0)
+    if isinstance(result, Exception):
+        raise result
     return result
 
 
