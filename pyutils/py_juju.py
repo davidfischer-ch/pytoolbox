9d98bc5c0df31514a748338474bf4c160b696805
David Fischer <david.fischer.ch@gmail.com>
2013-08-30 08:41:33 +0200

    Add CharmConfig and CharmHooks classes

--- pyutils/py_juju.py
+++ pyutils/py_juju.py
@@ -24,7 +24,7 @@
 #
 #  Retrieved from git clone https://github.com/davidfischer-ch/pyutils.git
 
-import os, time, uuid, yaml
+import os, subprocess, sys, time, uuid, yaml
 from codecs import open
 from kitchen.text.converters import to_bytes
 from six import string_types
@@ -32,7 +32,39 @@ from py_console import confirm
 from py_exception import TimeoutError
 from py_subprocess import cmd
 
+try:
+    import charmhelpers
+except ImportError:
+    subprocess.check_call([u'apt-add-repository', u'-y', u'ppa:juju/pkgs'])
+    subprocess.check_call([u'apt-get', u'update'])
+    subprocess.check_call([u'apt-get', u'install', u'-y', u'python-charmhelpers'])
+    import charmhelpers
+
+from shelltoolbox import command
+
+
 DEFAULT_ENVIRONMENTS_FILE = os.path.abspath(os.path.expanduser(u'~/.juju/environments.yaml'))
+DEFAULT_OS_ENV = {
+    u'APT_LISTCHANGES_FRONTEND': u'none',
+    u'CHARM_DIR': u'/var/lib/juju/units/oscied-storage-0/charm',
+    u'DEBIAN_FRONTEND': u'noninteractive',
+    u'_JUJU_CHARM_FORMAT': u'1',
+    u'JUJU_AGENT_SOCKET': u'/var/lib/juju/units/oscied-storage-0/.juju.hookcli.sock',
+    u'JUJU_CLIENT_ID': u'constant',
+    u'JUJU_ENV_UUID': u'878ca8f623174911960f6fbed84f7bdd',
+    u'JUJU_PYTHONPATH': u':/usr/lib/python2.7/dist-packages:/usr/lib/python2.7'
+                        u':/usr/lib/python2.7/plat-x86_64-linux-gnu'
+                        u':/usr/lib/python2.7/lib-tk'
+                        u':/usr/lib/python2.7/lib-old'
+                        u':/usr/lib/python2.7/lib-dynload'
+                        u':/usr/local/lib/python2.7/dist-packages'
+                        u':/usr/lib/pymodules/python2.7',
+    u'_': u'/usr/bin/python',
+    u'JUJU_UNIT_NAME': u'oscied-storage/0',
+    u'PATH': u'/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin',
+    u'PWD': u'/var/lib/juju/units/oscied-storage-0/charm',
+    u'SHLVL': u'1'
+}
 STARTED_STATES = (u'started',)
 ERROR_STATES = (u'not-started', u'error')
 
@@ -348,7 +380,321 @@ def remove_relation(environment, service1, service2, relation1=None, relation2=N
         if not u'exists' in unicode(e):
             raise
 
+
 # Helpers --------------------------------------------------------------------------------------------------------------
+
+__get_ip = None
+
+
+def get_ip():
+    global __get_ip
+    if __get_ip is None:
+        import socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((u'8.8.8.8', 80))
+        __get_ip = s.getsockname()[0]
+        s.close()
+    return __get_ip
+
+
+class CharmConfig(object):
+
+    def __init__(self):
+        self.verbose = False
+
+    def __repr__(self):
+        return unicode(self.__dict__)
+
+
+class CharmHooks(object):
+    u"""
+    A base class to build charms based on python hooks unit-testable even if juju is not installed.
+
+    The following attributes are set by ``__init__``: TODO
+
+    * ``local_config`` must be set by derived class to an instance of ``py_serialization.PickelableObject``.
+    This should be loaded from a file that is local to the unit by ``__init__``. This file is used to store service/
+    unit-specific configuration. In EBU's project called OSCIED, this file is even read by the encapsulated python
+    code of the worker (celery daemon).
+
+    **Example usage**:
+
+    >>> class MyCharmHooks(CharmHooks):
+    ...
+    ...     def hook_install(self):
+    ...         self.debug(u'hello world, install some packages with self.cmd(...)')
+    ...
+    ...     def hook_config_changed(self):
+    ...         self.remark(u'update services based on self.config and update self.local_config')
+    ...
+    ...     def hook_start(self):
+    ...         self.info(u'start services')
+    ...
+    ...     def hook_stop(self):
+    ...         self.info(u'stop services')
+    ...
+
+    Trigger some hooks:
+
+    >>> my_hooks = MyCharmHooks(u'metadata.yaml', u'config.yaml', DEFAULT_OS_ENV, force_disable_juju=True)
+    [DEBUG] Load metadatas from file metadata.yaml
+
+    >>> my_hooks.trigger(u'install')
+    [HOOK] Execute MyCharmHooks hook install
+    [DEBUG] hello world, install some packages with self.cmd(...)
+
+    >>> my_hooks.trigger(u'config-changed')
+    [HOOK] Execute MyCharmHooks hook config-changed
+    [REMARK] update services based on self.config and update self.local_config !
+
+    >>> my_hooks.trigger(u'start')
+    [HOOK] Execute MyCharmHooks hook start
+    [INFO] start services
+
+    >>> my_hooks.trigger(u'stop')
+    [HOOK] Execute MyCharmHooks hook stop
+    [INFO] stop services
+
+    >>> my_hooks.trigger(u'not_exist')
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'MyCharmHooks' object has no attribute 'hook_not_exist'
+    """
+
+    def __init__(self, metadata, default_config, default_os_env, force_disable_juju=False):
+        self.config = CharmConfig()
+        self.local_config = None
+        try:
+            if force_disable_juju:
+                raise OSError()
+            self.juju_ok = True
+            self.juju_log = command(u'juju-log')
+            self.load_config(charmhelpers.get_config())
+            self.env_uuid = os.environ.get(u'JUJU_ENV_UUID')
+            self.name = os.environ[u'JUJU_UNIT_NAME']
+            self.private_address = charmhelpers.unit_get(u'private-address')
+            self.public_address = charmhelpers.unit_get(u'public-address')
+        except (subprocess.CalledProcessError, OSError):
+            self.juju_ok = False
+            self.juju_log = command(u'echo')
+            if default_config is not None:
+                self.load_config(default_config)
+            self.env_uuid = default_os_env[u'JUJU_ENV_UUID']
+            self.name = default_os_env[u'JUJU_UNIT_NAME']
+            self.private_address = self.public_address = get_ip()
+        self.load_metadata(metadata)
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    @property
+    def id(self):
+        u"""
+        Returns the id extracted from the unit's name.
+
+        **Example usage**:
+
+        >>> hooks = CharmHooks(None, None, DEFAULT_OS_ENV)
+        >>> hooks.name = u'oscied-storage/3'
+        >>> hooks.id
+        3
+        """
+        return int(self.name.split(u'/')[1])
+
+    @property
+    def is_leader(self):
+        u"""
+        Returns True if current unit is the leader of the peer-relation.
+
+        By convention, the leader is the unit with the *smallest* id (the *oldest* unit).
+        """
+        try:
+            ids = [int(i.split(u'/')[1]) for i in self.relation_list(None)]
+            self.debug(u'id={0} ids={1}'.format(self.id, ids))
+            return len(ids) == 0 or self.id <= min(ids)
+        except Exception as e:
+            self.debug(u'Bug during leader detection: {0}'.format(repr(e)))
+            return True
+
+    # Maps calls to charm helpers methods and replace them if called in standalone -------------------------------------
+
+    def log(self, message):
+        if self.juju_ok:
+            return charmhelpers.log(message, self.juju_log)
+        print(message)
+        return None
+
+    def open_port(self, port, protocol=u'TCP'):
+        if self.juju_ok:
+            charmhelpers.open_port(port, protocol)
+        else:
+            self.debug(u'Open port {0} ({1})'.format(port, protocol))
+
+    def close_port(self, port, protocol=u'TCP'):
+        if self.juju_ok:
+            charmhelpers.close_port(port, protocol)
+        else:
+            self.debug(u'Close port {0} ({1})'.format(port, protocol))
+
+    def unit_get(self, attribute):
+        if self.juju_ok:
+            return charmhelpers.unit_get(attribute)
+        raise NotImplementedError(to_bytes(u'FIXME unit_get not yet implemented'))
+
+    def relation_get(self, attribute=None, unit=None, rid=None):
+        if self.juju_ok:
+            return charmhelpers.relation_get(attribute, unit, rid)
+        raise NotImplementedError(to_bytes(u'FIXME relation_get not yet implemented'))
+
+    def relation_ids(self, relation_name):
+        if self.juju_ok:
+            return [int(id) for id in charmhelpers.relation_ids(relation_name)]
+        raise NotImplementedError(to_bytes(u'FIXME relation_ids not yet implemented'))
+
+    def relation_list(self, rid=None):
+        if self.juju_ok:
+            return charmhelpers.relation_list(rid)
+        raise NotImplementedError(to_bytes(u'FIXME relation_list not yet implemented'))
+
+    def relation_set(self, **kwargs):
+        if self.juju_ok:
+            charmhelpers.relation_set(**kwargs)
+        else:
+            raise NotImplementedError(to_bytes(u'FIXME relation_set not yet implemented'))
+
+    # Convenience methods for logging ----------------------------------------------------------------------------------
+
+    def debug(self, message):
+        u"""
+        Convenience method for logging a debug-related message.
+        """
+        if self.config.verbose:
+            return self.log(u'[DEBUG] {0}'.format(message))
+
+    def info(self, message):
+        u"""
+        Convenience method for logging a standard message.
+        """
+        return self.log(u'[INFO] {0}'.format(message))
+
+    def hook(self, message):
+        u"""
+        Convenience method for logging the triggering of a hook.
+        """
+        return self.log(u'[HOOK] {0}'.format(message))
+
+    def remark(self, message):
+        u"""
+        Convenience method for logging an important remark.
+        """
+        return self.log(u'[REMARK] {0} !'.format(message))
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def load_config(self, config):
+        u"""
+        Updates ``config`` attribute with given configuration.
+
+        **Example usage**:
+
+        >>> hooks = CharmHooks(None, None, DEFAULT_OS_ENV)
+        >>> hasattr(hooks.config, u'pingu') or hasattr(hooks.config, u'rabbit_password')
+        False
+        >>> hooks.load_config({u'pingu': u'bi bi'})
+        >>> print(hooks.config.pingu)
+        bi bi
+        >>> hooks.config.verbose = True
+        >>> hooks.load_config(u'config.yaml')  # doctest: +ELLIPSIS
+        [DEBUG] Load config from file config.yaml
+        [DEBUG] Convert boolean option verbose true -> True
+        [DEBUG] Convert boolean option email_tls true -> True
+        [DEBUG] Convert boolean option cleanup true -> True
+        >>> hasattr(hooks.config, u'rabbit_password')
+        True
+        """
+        self.config.__dict__.update(load_unit_config(config, log=self.debug))
+
+    def save_local_config(self):
+        u"""
+        Save or update local configuration file only if this instance has the attribute
+        ``local_config``.
+        """
+        if self.local_config is not None:
+            self.debug(u'Save (updated) local configuration {0}'.format(self.local_config))
+            self.local_config.write()
+
+    def load_metadata(self, metadata):
+        u"""
+        Set ``metadata`` attribute with given metadatas, ``metadata`` can be:
+
+        * The filename of a charm metadata file (e.g. ``metadata.yaml``)
+        * A dictionary containing the metadatas.
+
+        **Example usage**:
+
+        >>> hooks = CharmHooks(None, None, DEFAULT_OS_ENV, force_disable_juju=True)
+        >>> hooks.metadata
+        >>> hooks.load_metadata({u'ensemble': u'oscied'})
+        >>> hooks.metadata
+        {u'ensemble': u'oscied'}
+        >>> hooks.config.verbose = True
+        >>> hooks.load_metadata(u'metadata.yaml')  # doctest: +ELLIPSIS
+        [DEBUG] Load metadatas from file metadata.yaml
+        >>> print(hooks.metadata[u'maintainer'])
+        OSCIED Main Developper <david.fischer.ch@gmail.com>
+        """
+        if isinstance(metadata, string_types):
+            self.debug(u'Load metadatas from file {0}'.format(metadata))
+            with open(metadata, u'r', u'utf-8') as f:
+                metadata = yaml.load(f)
+        self.metadata = metadata
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def cmd(self, command, input=None, cli_input=None, fail=True):
+        u"""
+        Calls the ``command`` and returns a dictionary with stdout, stderr, and the returncode.
+
+        .. seealso:: :mod:`pyutils`
+        """
+        return cmd(command, input=input, cli_input=cli_input, fail=fail, log=self.debug)
+
+    def template2config(self, template, config, values):
+        with open(template, u'r', u'utf-8') as template_file:
+            data = template_file.read()
+            data = data.format(**values)
+            with open(config, u'w', u'utf-8') as config_file:
+                config_file.write(data)
+                self.remark(u'File {0} successfully generated'.format(config))
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def trigger(self, hook_name=None):
+        u"""
+        Triggers a hook specified in ``hook_name``, defaults to ``sys.argv[1]``.
+
+        Hook's name is the nice hook name that one can find in official juju documentation.
+        For example if ``config-changed`` is mapped to a call to ``self.hook_config_changed()``.
+
+        A ``ValueError`` containing a usage string is raised if a bad number of argument is given.
+        """
+        if hook_name is None:
+            if len(sys.argv) != 2:
+                raise ValueError(to_bytes(u'Usage {0} hook_name (e.g. config-changed)'.format(sys.argv[0])))
+            hook_name = sys.argv[1]
+
+        if self.juju_ok:
+            charmhelpers.log_entry()
+        try:  # Call the function hooks_...
+            self.hook(u'Execute {0} hook {1}'.format(self.__class__.__name__, hook_name))
+            getattr(self, u'hook_{0}'.format(hook_name.replace(u'-', u'_')))()
+            self.save_local_config()
+        except subprocess.CalledProcessError as e:
+            self.log(u'Exception caught:')
+            self.log(e.output)
+            raise
+        finally:
+            if self.juju_ok:
+                charmhelpers.log_exit()
 
 from functools import wraps
 
