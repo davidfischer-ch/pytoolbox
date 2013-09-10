0f1727fd4f9acd6675b48e2f365872dc910e9960
David Fischer <david.fischer.ch@gmail.com>
2013-09-10 12:27:34 +0200

    Try to fix Travis CI

--- tests/runtests.py
+++ tests/runtests.py
@@ -34,7 +34,7 @@ def main():
             from py_unittest import runtests
         except ImportError:
             from pyutils.py_unittest import runtests
-        return runtests(__file__, cover_packages=[u'pyutils'], packages=[u'pyutils'], ignore='py_django.py')
+        return runtests(__file__, cover_packages=[u'pyutils'], packages=[u'pyutils'], ignore='py_django.py|__init__.py')
     except ImportError:
         print(sys.path)
         raise
