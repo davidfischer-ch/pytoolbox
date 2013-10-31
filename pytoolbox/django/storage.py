7dc94770322c0ea8ec3749424515ef6493dca1f5
David Fischer <david.fischer.ch@gmail.com>
2013-10-31 11:00:10 +0100

    Add OverwriteMixin

---
+++ pytoolbox/django/storage.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+#**********************************************************************************************************************#
+#                                       PYUTILS - TOOLBOX FOR PYTHON SCRIPTS
+#
+#  Main Developer : David Fischer (david.fischer.ch@gmail.com)
+#  Copyright      : Copyright (c) 2012-2013 David Fischer. All rights reserved.
+#
+#**********************************************************************************************************************#
+#
+# This file is part of David Fischer's pytoolbox Project.
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the EUPL v. 1.1 as provided
+# by the European Commission. This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the European Union Public License for more details.
+#
+# You should have received a copy of the EUPL General Public License along with this project.
+# If not, see he EUPL licence v1.1 is available in 22 languages:
+#     22-07-2013, <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>
+#
+# Retrieved from https://github.com/davidfischer-ch/pytoolbox.git
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+from django.conf import settings
+from ..filesystem import try_remove
+
+
+class OverwriteMixin(object):
+    u"""Update get_available_name to remove any previously stored file (if any) before returning the name."""
+
+    def get_available_name(self, name):
+        try_remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
