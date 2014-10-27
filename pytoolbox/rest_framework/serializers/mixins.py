7cc2617a826a57d4613f23ae2c68e8d50e4b5993
David Fischer <david.fischer.ch@gmail.com>
2014-10-27 15:20:07 +0100

    Add class rest_framework.serializers.mixins.FromPrivateKeyMixin

--- pytoolbox/rest_framework/serializers/mixins.py
+++ pytoolbox/rest_framework/serializers/mixins.py
@@ -24,7 +24,11 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__all__ = ('DisableValidateOnSaveMixin', )
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.utils.translation import ugettext_lazy as _
+from rest_framework.compat import smart_text
+
+__all__ = ('DisableValidateOnSaveMixin', 'FromPrivateKeyMixin')
 
 
 class DisableValidateOnSaveMixin(object):
@@ -36,3 +40,29 @@ class DisableValidateOnSaveMixin(object):
     def save_object(self, obj, **kwargs):
         kwargs['validate'] = False
         return super(DisableValidateOnSaveMixin, self).save_object(obj, **kwargs)
+
+
+class FromPrivateKeyMixin(object):
+    """Allow to provide the PK of the model to retrieve it instead of creating a new instance with fields from data."""
+
+    default_error_messages = {
+        'does_not_exist': _("Invalid pk '%s' - object does not exist."),
+        'incorrect_type': _('Incorrect type.  Expected pk value, received %s.'),
+    }
+
+    def from_native(self, data, files=None):
+        if not data or isinstance(data, dict):
+            return super().from_native(data, files)
+        try:
+            obj = self.opts.model.objects.get(pk=data)
+            obj._from_pk = True
+            return obj
+        except ObjectDoesNotExist:
+            raise ValidationError(self.error_messages['does_not_exist'] % smart_text(data))
+        except (TypeError, ValueError):
+            received = type(data).__name__
+            raise ValidationError(self.error_messages['incorrect_type'] % received)
+
+    def save_object(self, obj, **kwargs):
+        if not getattr(obj, '_from_pk', False):
+            obj.save(**kwargs)
