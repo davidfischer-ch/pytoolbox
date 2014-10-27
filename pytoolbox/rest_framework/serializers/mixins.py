42f1dfb1314ea8a73380e9f6855ab6ef9ca705cf
David Fischer <david.fischer.ch@gmail.com>
2014-10-27 13:50:27 +0100

    Add module rest_framework

---
+++ pytoolbox/rest_framework/serializers/mixins.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+#**********************************************************************************************************************#
+#                                        PYTOOLBOX - TOOLBOX FOR PYTHON SCRIPTS
+#
+#  Main Developer : David Fischer (david.fischer.ch@gmail.com)
+#  Copyright      : Copyright (c) 2012-2014 David Fischer. All rights reserved.
+#
+#**********************************************************************************************************************#
+#
+# This file is part of David Fischer's pytoolbox Project.
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the EUPL v. 1.1 as provided
+# by the European Commission. This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the European Union Public License for more details.
+#
+# You should have received a copy of the EUPL General Public License along with this project.
+# If not, see he EUPL licence v1.1 is available in 22 languages:
+#     22-07-2013, <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>
+#
+# Retrieved from https://github.com/davidfischer-ch/pytoolbox.git
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__all__ = ('DisableValidateOnSaveMixin', )
+
+
+class DisableValidateOnSaveMixin(object):
+    """
+    Disable call of obj.full_clean() by obj.save() because django-rest-framework views already calls it.
+    For example, use it if your model inherit from :mod:`pytoolbox.django.models.mixins.ValidateOnSaveMixin`.
+    """
+
+    def save_object(self, obj, **kwargs):
+        kwargs['validate'] = False
+        return super(DisableValidateOnSaveMixin, self).save_object(obj, **kwargs)
