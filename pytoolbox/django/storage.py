5708e2df0fca8758f8417064cc7b5614a87b86e4
David Fischer <david.fischer.ch@gmail.com>
2015-02-12 10:46:43 +0100

    Add django.storage.ExpressTemporaryFileMixin

--- pytoolbox/django/storage.py
+++ pytoolbox/django/storage.py
@@ -24,9 +24,29 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging, time
 from django.core.files.storage import FileSystemStorage
 
 __all__ = ('OverwriteMixin', 'OverwriteFileSystemStorage')
+
+logger = logging.getLogger(__name__)
+
+
+class ExpressTemporaryFileMixin(object):
+
+    def _save(self, name, content):
+        """
+        Save the temporary file to the storage.
+        Set temporary file path to allow using rename instead of chunked copy when possible.
+
+        See what happens in super()._save(): https://github.com/django/django/blob/master/django/core/files/storage.py
+        """
+        start_time = time.time()
+        if hasattr(content.file, 'temporary_file_path'):
+            content.temporary_file_path = lambda: content.file.temporary_file_path()
+        result = super(ExpressTemporaryFileMixin, self)._save(name, content)
+        logger.debug('Saved protected file "{0}" in {1:.2g} seconds'.format(name, time.time() - start_time))
+        return result
 
 
 class OverwriteMixin(object):
