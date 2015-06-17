6158bae3caf3c74134a26dc5f425148797bfbf48
David Fischer <david.fischer.ch@gmail.com>
2015-06-17 07:38:26 +0000

    Add test for django utils

---
+++ tests/test_django_utils.py
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+
+#**********************************************************************************************************************#
+#                                        PYTOOLBOX - TOOLBOX FOR PYTHON SCRIPTS
+#
+#  Main Developer : David Fischer (david.fischer.ch@gmail.com)
+#  Copyright      : Copyright (c) 2012-2015 David Fischer. All rights reserved.
+#
+#**********************************************************************************************************************#
+#
+# This file is part of David Fischer's pytoolbox Project.
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the EUPL v. 1.1 as provided
+# by the European Commission. This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the European Union Public License for more details.
+#
+# You should have received a copy of the EUPL General Public License along with this project.
+# If not, see he EUPL licence v1.1 is available in 22 languages:
+#     22-07-2013, <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>
+#
+# Retrieved from https://github.com/davidfischer-ch/pytoolbox.git
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import unittest
+from pytoolbox.django.utils import collections
+from pytoolbox.unittest import FilterByTagsMixin
+
+
+class TestDjangoUtils(FilterByTagsMixin, unittest.TestCase):
+
+    tags = ('django', 'utils')
+
+    def test_FieldsToValuesLookupDict(self):
+        class File(object):
+            pass
+
+        class Media(object):
+            pass
+
+        class MediaForm(object):
+            class Meta:
+                model = Media
+
+        numbers = collections.FieldsToValuesLookupDict('numbers', {'MediaForm.name': 1, 'Media.url': 2, 'url': 3})
+        self.assertEqual(numbers[(File, 'url')], 3)
+        self.assertEqual(numbers[(Media, 'url')], 2)
+        self.assertEqual(numbers[(MediaForm, 'url')], 2)
+        self.assertEqual(numbers[(MediaForm, 'name')], 1)
