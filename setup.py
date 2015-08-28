aa81efadff6144d264f3da6a6cec24795706575f
David Fischer <david.fischer.ch@gmail.com>
2015-08-28 10:59:19 +0000

    Release

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ if len(sys.argv) > 1 and sys.argv[1] in ('develop', 'install', 'test'):
 
 setup(
     name='pytoolbox',
-    version='10.2.0',
+    version='10.3.0',
     packages=find_packages(exclude=['tests']),
     extras_require=extras_require,
     install_requires=install_requires,
