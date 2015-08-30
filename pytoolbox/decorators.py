0372e53d0f2c22ae460d40396455446dff463e1d
David Fischer <david.fischer.ch@gmail.com>
2015-08-30 21:33:53 +0200

    Add cached_property decorator

--- pytoolbox/decorators.py
+++ pytoolbox/decorators.py
@@ -33,6 +33,27 @@ from .subprocess import cmd
 _all = module.All(globals())
 
 
+class cached_property(object):
+    """
+    Decorator that converts a method with a single self argument into a property cached on the instance.
+
+    Optional ``name`` argument allows you to make cached properties of other methods.
+    For example ``url=cached_property(get_absolute_url, name='url')``.
+
+    Copyright: Django Project.
+    """
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, '__doc__')
+        self.name = name or func.__name__
+
+    def __get__(self, instance, type=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
+
+
 def confirm_it(message, default=False, abort_message='Operation aborted by the user'):
     """Ask for confirmation before calling the decorated function."""
     def _confirm_it(f):
