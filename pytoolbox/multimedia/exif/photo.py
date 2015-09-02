fd15924a68f356e13c25676a50bfa04ccddaf72b
David Fischer <david.fischer.ch@gmail.com>
2015-09-02 16:42:23 +0200

    Add Orientation enum

--- pytoolbox/multimedia/exif/photo.py
+++ pytoolbox/multimedia/exif/photo.py
@@ -32,8 +32,20 @@ try:
         AUTO = 0
         MANUAL = 1
         BRACKET = 2
+
+    class Orientation(OrderedEnum):
+        NORMAL = 1
+        HOR_FLIP = 2
+        ROT_180_CCW = 3
+        VERT_FLIP = 4
+        HOR_FLIP_ROT_270_CW = 5
+        ROT_90_CW = 6
+        HOR_FLIP_ROT_90_CW = 7
+        ROT_270_CW = 8
+
 except ImportError:
     ExposureMode = {0: 'auto', 1: 'manual', 2: 'bracket'}.get
+    Orientation = lambda x: range(1, 9)[x-1]
 
 from ... import module
 
@@ -89,8 +101,11 @@ class Photo(object):
 
     @property
     def orientation(self):
-        orientation = self.metadata.exiv2.get_orientation()
-        return orientation.value_nick if orientation else None
+        data = self.metadata['Exif.Image.Orientation'].data
+        try:
+            return Orientation(data)
+        except:
+            pass
 
     @property
     def sensing_method(self):
