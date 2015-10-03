beed022604f34dc50ef5cc395278293654b42d06
David Fischer <david.fischer.ch@gmail.com>
2015-10-03 21:46:10 +0000

    Update doc tools

--- docs/update.py
+++ docs/update.py
@@ -25,37 +25,50 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import shutil, sys
+import os, shutil, sys
+
 from pytoolbox.encoding import configure_unicode
 from pytoolbox.subprocess import cmd
-from pytoolbox.filesystem import from_template
+from pytoolbox.filesystem import find_recursive, from_template
+from pytoolbox.string import to_lines
 
-configure_unicode()
+PACKAGE_DIRECTORY = os.path.join('..', 'pytoolbox')
 
-# Detect modules, thanks to find !
-modules = sorted(m.replace('.py', '').replace('./', '').replace('/', '.')
-                 for m in cmd('find . -type f -name "*.py"', cwd='../pytoolbox', shell=True)['stdout'].split()
-                 if m.endswith('.py') and not '__init__' in m)
 
-print('Detected modules are: {0}'.format(modules))
+def main():
+    configure_unicode()
 
-api_toc = ''
-for module in modules:
-    if 'django' in module or 'crypto' in module:
-        continue  # FIXME temporary hack, see issue #6
-    module = 'pytoolbox.{0}'.format(module)
-    title = module.replace('.', ' > ')
-    api_toc += '    {0}\n'.format(module)
-    from_template('templates/module.rst.template', 'source/{0}.rst'.format(module),
-                  {'module': module, 'title': title, 'equals': '='*len(title)})
+    # Cleanup previously generated restructured files
+    for path in find_recursive('source', r'^pytoolbox.*\.rst$', unix_wildcards=False):
+        os.remove(path)
 
-from_template('templates/api.rst.template', 'source/api.rst', {'api_toc': api_toc})
-shutil.rmtree('build/html', ignore_errors=True)
-result = cmd('make html', fail=False)
+    submodules = sorted(
+        m.replace('.py', '').replace(PACKAGE_DIRECTORY, '').strip(os.path.sep).replace(os.path.sep, '.')
+        for m in find_recursive(PACKAGE_DIRECTORY, r'[^_].*\.py$', unix_wildcards=False)
+    )
 
-print('\nOutputs\n=======\n')
-print(result['stdout'])
-print('\nErrors\n======\n')
-print(result['stderr'])
+    print('Detected modules are: {0}'.format(to_lines(submodules)))
 
-sys.exit(0 if not result['stderr'] else 1)
+    modules = []
+    for submodule in submodules:
+        if 'django' in submodule or 'crypto' in submodule:
+            continue  # FIXME temporary hack, see issue #6
+        module = 'pytoolbox.{0}'.format(submodule)
+        title = module.replace('.', ' > ')
+        modules.append(module)
+        from_template(os.path.join('templates', 'module.rst.template'), os.path.join('source', module + '.rst'), {
+            'module': module, 'title': title, 'equals': '='*len(title)
+        })
+
+    from_template(os.path.join('templates', 'api.rst.template'), os.path.join('source', 'api.rst'), {
+        'api_toc': os.linesep.join('    ' + m for m in modules)
+    })
+    shutil.rmtree(os.path.join('build', 'html'), ignore_errors=True)
+    result = cmd('make html', fail=False)
+
+    print('{0}Outputs{0}======={0}{0}{1[stdout]}{0}{0}Errors{0}======{0}{0}{1[stderr]}'.format(os.linesep, result))
+
+    sys.exit(1 if result['stderr'] else 0)
+
+if __name__ == '__main__':
+    main()
