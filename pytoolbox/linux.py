383f5e160458f3b8a94c60e5f7413c3ad9c91970
David Fischer <david.fischer.ch@gmail.com>
2016-02-16 09:12:59 +0100

    Add module linux

---
+++ pytoolbox/linux.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import configparser, os, re
+from codecs import open
+
+CONFIG_PREFIX = re.compile(r'^config_')
+DRIVER_IN_KERNEL = 'y'
+DRIVER_HAS_MODULE = 'm'
+
+
+def get_kernel_config(release=None, fail=True):
+    """
+    Return a JSON string with the GNU/Linux Kernel configuration.
+
+    **Example usage**
+
+    >>> from pytoolbox.unittest import asserts
+    >>> config = get_kernel_config(fail=False)
+    >>> asserts.is_instance(config, dict)
+    >>> asserts.assert_in('memory', config) if config else None
+
+    Error handling:
+
+    >>> get_kernel_config('0.0.1-generic', fail=False)
+    {}
+    """
+    # On Python<2.3.3 os.uname returns a tuple, so we stuck with it
+    try:
+        with open('/boot/config-{0}'.format(release or os.uname()[2])) as f:
+            config = configparser.ConfigParser()
+            config.read_string('[kernel]' + f.read())
+    except IOError:
+        if fail:
+            raise
+        return {}
+    return {CONFIG_PREFIX.sub('', k): v for k, v in config['kernel'].items()}
