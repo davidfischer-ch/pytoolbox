8850770c0b526b482f454ce16dbe9b118b44e830
David Fischer <david.fischer.ch@gmail.com>
2018-06-05 20:40:26 +0200

    Remove headers

--- tests/test_django_utils.py
+++ tests/test_django_utils.py
@@ -1,27 +1,5 @@
 # -*- encoding: utf-8 -*-
 
-#**********************************************************************************************************************#
-#                                        PYTOOLBOX - TOOLBOX FOR PYTHON SCRIPTS
-#
-#  Main Developer : David Fischer (david.fischer.ch@gmail.com)
-#  Copyright      : Copyright (c) 2012-2015 David Fischer. All rights reserved.
-#
-#**********************************************************************************************************************#
-#
-# This file is part of David Fischer's pytoolbox Project.
-#
-# This project is free software: you can redistribute it and/or modify it under the terms of the EUPL v. 1.1 as provided
-# by the European Commission. This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# See the European Union Public License for more details.
-#
-# You should have received a copy of the EUPL General Public License along with this project.
-# If not, see he EUPL licence v1.1 is available in 22 languages:
-#     22-07-2013, <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>
-#
-# Retrieved from https://github.com/davidfischer-ch/pytoolbox.git
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from pytoolbox.django.utils import collections
@@ -44,7 +22,8 @@ class TestDjangoUtils(base.TestCase):
             class Meta:
                 model = Media
 
-        numbers = collections.FieldsToValuesLookupDict('numbers', {'MediaForm.name': 1, 'Media.url': 2, 'url': 3})
+        numbers = collections.FieldsToValuesLookupDict(
+            'numbers', {'MediaForm.name': 1, 'Media.url': 2, 'url': 3})
         self.equal(numbers[(File, 'url')], 3)
         self.equal(numbers[(Media, 'url')], 2)
         self.equal(numbers[(MediaForm, 'url')], 2)
