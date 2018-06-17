014ce90f24eb3aaade207bd02b93ec4ff84cbf9e
David Fischer <david.fischer.ch@gmail.com>
2018-06-17 23:17:07 +0200

    Use E

--- pytoolbox/ai/vision/face/recognize/nn4_small2.py
+++ pytoolbox/ai/vision/face/recognize/nn4_small2.py
@@ -30,7 +30,7 @@ def conv2d_bn(
 ):
     num = '' if cv2_out is None else '1'
     tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, name=layer + '_conv' + num)(x)
-    tensor = BatchNormalization(axis=3, epsilon=0.00001, name=layer + '_bn' + num)(tensor)
+    tensor = BatchNormalization(axis=3, epsilon=E, name=layer + '_bn' + num)(tensor)
     tensor = Activation('relu')(tensor)
     if padding is None:
         return tensor
@@ -38,7 +38,7 @@ def conv2d_bn(
     if cv2_out is None:
         return tensor
     tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, name=layer + '_conv2')(tensor)
-    tensor = BatchNormalization(axis=3, epsilon=0.00001, name=layer + '_bn2')(tensor)
+    tensor = BatchNormalization(axis=3, epsilon=E, name=layer + '_bn2')(tensor)
     tensor = Activation('relu')(tensor)
     return tensor
 
