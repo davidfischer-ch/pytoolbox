c15fca4d3f61ca07770aaeb9ecbf09926cafe600
David Fischer <david.fischer.ch@gmail.com>
2019-03-24 21:06:08 +0100

    Make all exif stuff available from exif module itself

--- pytoolbox/multimedia/exif/__init__.py
+++ pytoolbox/multimedia/exif/__init__.py
@@ -2,5 +2,11 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from .brand import *
+from .camera import *
+from .equipment import *
+from .image import *
+from .lens import *
 from .metadata import *
+from .photo import *
 from .tag import *
