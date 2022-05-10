096988352da3a2b546efb16586fc0f6d661695d6
David Fischer <david@fisch3r.net>
2022-05-10 08:53:09 +0200

    Update supported Python versions (3.7 to 3.10)

--- setup.py
+++ setup.py
@@ -256,9 +256,10 @@ setuptools.setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
