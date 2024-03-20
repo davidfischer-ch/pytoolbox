baf6996a6fbe2b0cbb6961ee12332e712812e6d9
David Fischer <david@fisch3r.net>
2024-03-20 14:32:25 +0100

    Remove useless parameter

--- pytoolbox/module.py
+++ pytoolbox/module.py
@@ -7,9 +7,9 @@ __all__ = ['All']
 
 class All(object):  # pylint:disable=too-few-public-methods
 
-    def __init__(self, globals_) -> None:
-        self.init_keys = set(globals_.keys())
+    def __init__(self, globals_: dict[str, Any]) -> None:
+        self.init_keys: set[str] = set(globals_.keys())
 
-    def diff(self, globals_: dict[str, Any], to_type: type = list) -> list[str]:
+    def diff(self, globals_: dict[str, Any]) -> list[str]:
         new_keys = set(globals_.keys()) - self.init_keys
-        return to_type(k for k in new_keys if k[0] != '_')
+        return [k for k in new_keys if k[0] != '_']
