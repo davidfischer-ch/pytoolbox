2e638a65e4567aa22b847b5ba4429e78a1d1ecd0
David Fischer <david@fisch3r.net>
2026-03-06 00:05:06 +0100

    Fix termcolor tests by clearing can_colorize cache between tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 from unittest.mock import patch
 
-from pytest import mark
 import termcolor
 
 from pytoolbox import console
@@ -16,12 +15,13 @@ def test_toggle_colors() -> None:
     assert 'FORCE_COLOR' not in console.toggle_colors(colorize=False)
 
 
-@mark.skipif('CI' in os.environ, reason='Strangely colors are forced in GitHub CI')
 def test_disable_colors_with_termcolor() -> None:
     with patch.dict(os.environ, console.toggle_colors(colorize=False), clear=True):
+        termcolor.can_colorize.cache_clear()
         assert termcolor.colored('test', 'green') == 'test'
 
 
 def test_enable_colors_with_termcolor() -> None:
     with patch.dict(os.environ, console.toggle_colors(colorize=True), clear=True):
+        termcolor.can_colorize.cache_clear()
         assert termcolor.colored('test', 'green') == '\x1b[32mtest\x1b[0m'
